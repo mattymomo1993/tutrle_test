612a6108b4c3983d71e64ba0aef61f32794b3bd3
Matthew Morris <matthewjmorris1993@gmail.com>
2020-10-02 14:17:29 -0400

    this is fun help make it better

---
+++ turtle_test.py
@@ -0,0 +1,20 @@
+# number = 10
+# while(number > 0):
+#   print(number)
+#   number = number -1
+ 
+# if (StopIteration):
+#  print('BlastOff!! i figured out first while loop and ending it')
+
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("green")
+t.width(2)
+for i in range(113):
+    for i in range(112):
+        t.fd(113)
+        t.lt(119)
+    t.fd(113)
+    t.lt(236)
